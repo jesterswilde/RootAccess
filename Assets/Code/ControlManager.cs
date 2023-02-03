ace60a65152d41dc1315f69e34dae192e9b94d7e
Corey Wolff <jesterswilde@gmail.com>
2023-02-02 22:14:36 -0800

    Fixed term bug for realizes.

--- Assets/Code/ControlManager.cs
+++ Assets/Code/ControlManager.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 public class ControlManager : MonoBehaviour
 {
     static ControlManager T;
-    [SerializeField]
-    TerminalGUI termGUIPrefab;
-    [SerializeField]
-    TerminalGUI termGUI;
     ControlMode currentMode;
     public static ControlMode Mode => T.currentMode;
     public static event Action<ControlMode> OnModeChange;
@@ -15,18 +11,8 @@ public class ControlManager : MonoBehaviour
 
     public static void EnterMode(ControlMode mode)
     {
-        if(T.currentMode == ControlMode.Terminal)
-        {
-            Destroy(T.termGUI.gameObject);
-        }
         T.currentMode = mode;
         OnModeChange?.Invoke(mode);
-        //if (mode == ControlMode.Terminal)
-        //    T.ViewTerminal();        
-    }
-    void ViewTerminal()
-    {
-        termGUI = Instantiate(termGUIPrefab, GameManager.Canvas.transform) .Attach(Terminal.T);
     }
     private void Update()
     {
